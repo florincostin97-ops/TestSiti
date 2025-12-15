fbd19747156e103fe9f55d474caccfaa8247b7ff
Dass <florincostin97@gmail.com>
2025-12-15 03:53:18 +0100

    Aggiungi Astro.congif.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://tuo-username.github.io',  // opzionale, ma utile per sitemap e link assoluti
+  base: '/TestSiti/',  // aggiungi questo se il sito sarà su https://tuo-username.github.io/nome-repo/
+});
